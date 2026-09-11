5e33f089ba8afc67317a3d7b233d668711fb1daf
logarta <175045996+loizecoutinho@users.noreply.github.com>
2026-09-11 17:24:01 -0300

    teste de SOMA/DIV de vetores
    arquivo teste da instrução MAC "SUM"  e "DIV" que implementamos para a ISA

---
+++ TESTESUM.asm
@@ -0,0 +1,21 @@
+.data 
+VETOR:  .word 1, 2, 3, 4, 5, 6
+SIZE:   .word 6
+TESTE: .word 6
+UM: .word 1
+
+.text
+MAIN:  	
+	LOCO 7
+	ECALL
+    SUBD UM
+    NEG
+	STOD SIZE
+	SUM VETOR
+    DIV TESTE
+	LOCO 1
+    
+
+
+END:	ECALL
+	HALT
